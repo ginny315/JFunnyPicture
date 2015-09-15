37be6cafdd6e227739a7bdab00b3a2a9b7267f71
NingYanGuo <gny315@163.com>
2015-09-15 22:06:08 +0800

    add test file

---
+++ test.js
@@ -0,0 +1,12 @@
+(function($){
+	$.fn.swapClass = function(class1,class2){
+		return this.each(function(){
+			var $element = $(this);
+			if($element.hasClass(class1)){
+				$element.removeClass(class1).addClass(class2);
+			}else if($element.hasClass(class2)){
+				$element.removeClass(class2).addClass(class1);
+			}
+		})
+	}
+})(jQuery);
