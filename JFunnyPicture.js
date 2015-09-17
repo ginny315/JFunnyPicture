89c302030e87aca2d5603713791ac33cc0f4edb0
NingYanGuo <gny315@163.com>
2015-09-17 23:45:04 +0800

    divide the picture follow i

--- JFunnyPicture.js
+++ JFunnyPicture.js
@@ -1,55 +1,55 @@
-$(document).ready(function(){
-	 	 	 $('img').on("mouseover",function(){
-	 	 	 	 var that = this;
-	 	 	 	 $(that).css('position','absolute');
-	 	 	 	 var width = that.width/2;
-	 	 	 	 var height = that.height/2;
-	 	 	 	 $('<div></div>').addClass('one_cover_1').css({
-	 	 	 	 	position: 'absolute',
-					width: width,
-					height: height,
-					backgroundColor: '#000',
-					opacity: 0,
-					zIndex :'9999'
-	 	 	 	 }).delay(1000).animate({
-	 	 	 	 	opacity: 0.3,
-	 	 	 	 },3000).insertAfter($(this).parent());
+// $(document).ready(function(){
+// 	 	 	 $('img').on("mouseover",function(){
+// 	 	 	 	 var that = this;
+// 	 	 	 	 $(that).css('position','absolute');
+// 	 	 	 	 var width = that.width/2;
+// 	 	 	 	 var height = that.height/2;
+// 	 	 	 	 $('<div></div>').addClass('one_cover_'+i).css({
+// 	 	 	 	 	position: 'absolute',
+// 					width: width,
+// 					height: height,
+// 					backgroundColor: '#000',
+// 					opacity: 0,
+// 					zIndex :'9999'
+// 	 	 	 	 }).delay(1000).animate({
+// 	 	 	 	 	opacity: 0.3,
+// 	 	 	 	 },3000).insertAfter($(this).parent());
+	 	 	 	 
+// 	 	 	 })
+// });
 
-	 	 	 	 $('<div class="one_cover_2"></div>').css({
-	 	 	 	 	position: 'absolute',
-					width: width,
-					height: height,
-					left:width,
-					backgroundColor: '#000',
-					opacity: 0,
-					zIndex :'9999'
-	 	 	 	 }).delay(1000).animate({
-	 	 	 	 	opacity: 0.5,
-	 	 	 	 },3000).insertAfter($(this).parent());
 
-	 	 	 	 $('<div class="one_cover_3"></div>').css({
-	 	 	 	 	position: 'absolute',
-					width: width,
-					height: height,
-					top:height,
-					backgroundColor: '#000',
-					opacity: 0,
-					zIndex :'9999'
-	 	 	 	 }).delay(1000).animate({
-	 	 	 	 	opacity: 0.5,
-	 	 	 	 },3000).insertAfter($(this).parent());
+(function($){
+	$.fn.JFunnyPicture = function(opts){
+		var options = $.extend({},$.fn.JFunnyPicture.defaults,opts);
+		return this.each(function(){
+			$(this).on('mouseenter',function(){
+				var that = this;
+	 	 	 	 //$(that).css('position','absolute');
+	 	 	 	 var n = Math.sqrt(options.n);
+	 	 	 	 var width = that.width/n;
+	 	 	 	 var height = that.height/n;
+	 	 	 	 for(var i=1,j=options.n+1 ; i<j ; i++){
+	 	 	 	 		console.log(i);
+		 	 	 	 $('<div></div>').addClass('one_cover_'+i).css({
+		 	 	 	 	
+		 	 	 	 	position: 'absolute',
+						width: width,
+						height: height,
+						left:(i+n-1)%n*width,
+						top:Math.floor((i-1)/n)*height,   
+						backgroundColor: '#000',
+						opacity: 0,
+						zIndex :'9999'
+		 	 	 	 }).delay(0).animate({
+		 	 	 	 	opacity: 0.1*i,
+		 	 	 	 },500).insertAfter($(this));
+	 	 	 	 }
+			});
+		});
+	}
 
-	 	 	 	 $('<div class="one_cover_4"></div>').css({
-	 	 	 	 	position: 'absolute',
-					width: width,
-					height: height,
-					left:width,
-					top:height,
-					backgroundColor: '#000',
-					opacity:0,
-					zIndex :'9999'
-	 	 	 	 }).animate({
-	 	 	 	 	opacity:0.8
-	 	 	 	 },3000).insertAfter($(this).parent());
-	 	 	 })
-	 	 });
+	$.fn.JFunnyPicture.defaults = {
+			n:4
+	}
+})(jQuery);
